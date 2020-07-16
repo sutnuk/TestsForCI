0d08aa14d05f28bf725aaadd51208ee3f63b0ffd
Nazar Sytnyk <Sutnuk23nazar@gmail.com>
2020-07-16 18:53:49 +0300

    Finished creating a simple test for CI

--- TestsForCI/Program.cs
+++ TestsForCI/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace TestsForCI
 {
@@ -10,6 +8,34 @@ namespace TestsForCI
     {
         static void Main(string[] args)
         {
+                        
         }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void PreConditions()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://ua.sinoptik.ua/";
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test()
+        {
+            IWebElement element = driver.FindElement(By.XPath("//input[@id='search_city']"));
+            Assert.True(element.GetAttribute("placeholder").Equals("Назва населеного пункту, країни або регіону"));
+        }
+
+        [TearDown]
+        public void PostConditions()
+        {
+            driver.Quit();
+        }
+
     }
 }
